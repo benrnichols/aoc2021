9a7105738b0798762e3f20ef7848d3433a0d372c
TB Nichols <benjamin.r.nichols@vanderbilt.edu>
2021-12-20 15:01:38 -0500

    forgot to save before committing

--- problem19/code.py
+++ problem19/code.py
@@ -8,6 +8,15 @@ for x in f:
     elif len(x) !=0:
         beacons.append([])
 
-
-
-
+def matchbeacons(beacons, pairs, ordering):
+    if len(pairs) == 12:
+        return pairs[0][0][0], pairs [0][1][0]
+    elif len(pairs) == 0:
+        #loop through all beacon pairs
+        pass
+    else:
+        beacon1 = pairs[0][0][0]
+        beacon2 = pairs[0][1][0]
+        for location in list(filter(lambda x: x not in [y[0][1] for y in pairs], beacons[beacon1])):
+            for location2 in list(filter(lambda x: x not in [y[1][1] for y in pairs], beacons[beacon2])):
+                
