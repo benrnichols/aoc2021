e98445f2ce85578b844f8b6f6f83f0a72df16843
TB Nichols <benjamin.r.nichols@vanderbilt.edu>
2021-12-21 20:20:56 -0500

    bad implementations for day 19

--- problem19/code2.py
+++ problem19/code2.py
@@ -1,7 +1,6 @@
 f = open('input.txt', 'r')
 
 beacons = []
-tried = set()
 possibleorderings = [((0,1),(1,1),(2,1)),((0,1),(1,-1),(2,-1)),((0,-1),(1,1),(2,-1)),((0,-1),(1,-1),(2,1)),((0,-1),(1,-1),(2,-1)),\
                      ((0,1),(2,1),(1,1)),((0,1),(2,1),(1,-1)),((0,1),(2,-1),(1,1)),((0,1),(2,-1),(1,-1)),((0,-1),(2,1),(1,1)),((0,-1),(2,1),(1,-1)),((0,-1),(2,-1),(1,1)),((0,-1),(2,-1),(1,-1)),\
                      ((1,1),(0,1),(2,1)),((1,1),(0,1),(2,-1)),((1,1),(0,-1),(2,1)),((1,1),(0,-1),(2,-1)),((1,-1),(0,1),(2,1)),((1,-1),(0,1),(2,-1)),((1,-1),(0,-1),(2,1)),((1,-1),(0,-1),(2,-1)),\
@@ -22,38 +21,34 @@ for x in f:
 # the members are tuples where the first element is the beacon number they are percieved from and the second element is the ordered pair
 #the orderings are a three tuple of tuples where the first element of each tuple is whether it represents x,y, or z (0,1,2) 
 # and the second is whether it is inverted or not (-1,1)
-def matchbeacons(beacons, pairs, ordering):
+def matchbeacons(baselist, beacons, pairs, ordering):
     if len(pairs) == 0:
         for i in range(len(beacons)):
-            for j in range(i):
-                if (tuple(beacons[i]), tuple(beacons[j])) in tried:
-                    print("short circuiting")
-                    continue
-                for k in range(len(beacons[i])):
-                    for l in range(len(beacons[j])):
+                for k in range(len(baselist)):
+                    for l in range(len(beacons[i])):
                         for order in possibleorderings:
-                            newpairobject=((i, beacons[i][k]), [j, beacons[j][l]])
-                            result = matchbeacons(beacons, [newpairobject], order)
+                            newpairobject=((None, baselist[k]), [i, beacons[i][l]])
+                            result = matchbeacons(baselist, beacons, [newpairobject], order)
                             if not isinstance(result, bool):
                                 return result
-                tried.add((tuple(beacons[i]), tuple(beacons[j])))
     else:
-        beacon1 = pairs[0][0][0]
+        beacon1 = baselist
         beacon2 = pairs[0][1][0]
         baseb1 = pairs[0][0][1]
         baseb2 = pairs[0][1][1]
-        diff1 = set(map(lambda x: tuple(map(lambda y: x[y]-baseb1[y], [z for z in range(3)])), beacons[beacon1]))
+        diff1 = set(map(lambda x: tuple(map(lambda y: x[y]-baseb1[y], [z for z in range(3)])), baselist))
         diff2 = set(map(lambda x: tuple(map(lambda y: (x[ordering[y][0]]-baseb2[ordering[y][0]])* ordering[y][1], [z for z in range(3)])),  beacons[beacon2]))
         if len(diff1.intersection(diff2))>=12:
             return pairs, ordering            
         return False
 
-
-while len(beacons)!=1:
+baselist = beacons.pop(0)
+scanners = [(0,0,0)]
+while len(beacons)!=0:
     print(len(beacons))
-    result = matchbeacons(beacons,[], None)
+    result = matchbeacons(baselist, beacons,[], None)
     ## unify the two beacons, using ordering to translate
-    beacon1 = beacons.pop(result[0][0][0][0])
+    beacon1 = baselist
     beacon2 = beacons.pop(result[0][0][1][0])
     ordering = result[1]
     ref1 = result[0][0][0][1]
@@ -61,7 +56,7 @@ while len(beacons)!=1:
     diffx = ref1[0] - ref2[ordering[0][0]]
     diffy = ref1[1] - ref2[ordering[1][0]]
     diffz = ref1[2] - ref2[ordering[2][0]]
-    for toplace in list(filter(lambda x: x not in [y[1][1] for y in result[0]], beacon2)):
+    for toplace in beacon2:
         calcx = ref1[0] + (toplace[ordering[0][0]] - ref2[ordering[0][0]])*ordering[0][1]
         calcy = ref1[1] + (toplace[ordering[1][0]] - ref2[ordering[1][0]])*ordering[1][1]
         calcz = ref1[2] + (toplace[ordering[2][0]] - ref2[ordering[2][0]])*ordering[2][1]
@@ -73,7 +68,17 @@ while len(beacons)!=1:
         # print(toplace)
         # print((calcx, calcy, calcz))
         if (calcx, calcy, calcz) not in beacon1:
-            beacon1.append((calcx, calcy, calcz))
-    beacons = [beacon1] + beacons
+            baselist.append((calcx, calcy, calcz))
+    sensorplace = (0,0,0)
+    calcx = ref1[0] + (sensorplace[ordering[0][0]] - ref2[ordering[0][0]])*ordering[0][1]
+    calcy = ref1[1] + (sensorplace[ordering[1][0]] - ref2[ordering[1][0]])*ordering[1][1]
+    calcz = ref1[2] + (sensorplace[ordering[2][0]] - ref2[ordering[2][0]])*ordering[2][1]
+    scanners.append((calcx, calcy, calcz))
+
+maxmandist = 0
+for i in range(len(scanners)):
+    for j in range(i):
+        maxmandist = max(maxmandist, (abs(scanners[i][0] - scanners[j][0]) + abs(scanners[i][1] - scanners[j][1])+ abs(scanners[i][2] - scanners[j][2])))
+print(maxmandist)
 print(len(beacons))
-print(len(beacons[0]))
+print(len(baselist))
